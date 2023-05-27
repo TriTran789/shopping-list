137f463055b5a8f64ba5964ad567b85a08496221
TriTran <thanhtritran8@gmail.com>
2023-05-27 23:21:12 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+function ShoppingList({ items, setItems }) {
+  
+
+  function handleClick(item) {
+    function deleteItem(a) {
+      return a !== item
+    }
+
+    setItems(items.filter(deleteItem))
+  }
+
+  const itemsList = []
+  items.map(item => {
+    itemsList.push(
+      <div className='item'>
+        <li className='item' key={item}>{item}</li> <i className="fa-solid fa-circle-xmark" onClick={() => handleClick(item)}></i>
+      </div>
+    )
+    return
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const form = e.target
+    const input = form.name
+    setItems([...items, input.value])
+  }
+
+  return (
+    <div className="shopping-list">
+      <h1 className="project-name">Project 4: Shopping List</h1>
+      <div className="shopping-box">
+        <h2 className="box-title">Items To Buy</h2>
+        <div className='box-body'>
+          <form className="add-item-form" onSubmit={handleSubmit}>
+            <input type="text" name='name' required placeholder="Add a new item" />
+            <button className="add-item-btn">Add</button>
+          </form>
+          <ul className="item-list">{itemsList}</ul>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [items, setItems] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShoppingList items={items} setItems={setItems} />
     </div>
   );
 }
